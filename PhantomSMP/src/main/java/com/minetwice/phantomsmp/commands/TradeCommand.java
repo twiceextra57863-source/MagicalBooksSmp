980dd7112e6d317ada9af5ba677a3c94be6ec2d5
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:17:27 +0530

    Create TradeCommand.java

---
+++ PhantomSMP/src/main/java/com/minetwice/phantomsmp/commands/TradeCommand.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TradeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+}
