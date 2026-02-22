6e5dce323d641a9277a2d9faa0f6baf0cd5fe16c
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 18:07:17 +0530

    Update PlayerQuitListener.java

--- src/main/java/com/minetwice/phantomsmp/listeners/PlayerQuitListener.java
+++ src/main/java/com/minetwice/phantomsmp/listeners/PlayerQuitListener.java
@@ -1,12 +1,27 @@
 package com.minetwice.phantomsmp.listeners;
 
+import com.minetwice.phantomsmp.PhantomSMP;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitListener implements Listener {
+    
+    private final PhantomSMP plugin;
+    
+    public PlayerQuitListener(PhantomSMP plugin) {
+        this.plugin = plugin;
+    }
+    
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        // Handle quit logic
+        Player player = event.getPlayer();
+        
+        // Save player data
+        plugin.getDataManager().savePlayerData(player.getUniqueId());
+        
+        // Stop particles
+        plugin.getParticleManager().stopParticles(player);
     }
 }
