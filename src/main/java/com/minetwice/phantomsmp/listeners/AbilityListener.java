34337d09d7cf0536e69ccff60f2104c8f6e94358
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:52:59 +0530

    Create AbilityListener.java

---
+++ src/main/java/com/minetwice/phantomsmp/listeners/AbilityListener.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class AbilityListener implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Handle abilities
+    }
+}
