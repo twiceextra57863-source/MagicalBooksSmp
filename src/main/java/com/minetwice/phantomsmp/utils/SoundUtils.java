b02637a69892db8b13e0098dba92a8975c5acdeb
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 18:15:00 +0530

    Update SoundUtils.java

--- src/main/java/com/minetwice/phantomsmp/utils/SoundUtils.java
+++ src/main/java/com/minetwice/phantomsmp/utils/SoundUtils.java
@@ -2,11 +2,37 @@ package com.minetwice.phantomsmp.utils;
 
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 
 public class SoundUtils {
-    public static void playSound(Location loc, Sound sound, float volume, float pitch) {
-        if (loc.getWorld() != null) {
-            loc.getWorld().playSound(loc, sound, volume, pitch);
+    
+    public static void playAbilitySound(Player player, Sound sound) {
+        if (sound != null) {
+            player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
         }
+    }
+    
+    public static void playGlobalSound(Location location, Sound sound, float volume, float pitch) {
+        location.getWorld().playSound(location, sound, volume, pitch);
+    }
+    
+    public static void playLevelUpSound(Player player) {
+        player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+    }
+    
+    public static void playGemAwakenSound(Player player) {
+        player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 0.5f);
+    }
+    
+    public static void playTradeSound(Player player) {
+        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
+    }
+    
+    public static void playErrorSound(Player player) {
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1.0f, 0.5f);
+    }
+    
+    public static void playSuccessSound(Player player) {
+        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
     }
 }
