96040c061152770ed52b06a651796301b0de0b3e
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:17:28 +0530

    Create DebugCommands.java

---
+++ PhantomSMP/src/main/java/com/minetwice/phantomsmp/commands/DebugCommands.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DebugCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+}
