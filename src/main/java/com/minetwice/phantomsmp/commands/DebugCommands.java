53835f0d42019ae9edf4dc83fb695794d701c15d
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:52:55 +0530

    Create DebugCommands.java

---
+++ src/main/java/com/minetwice/phantomsmp/commands/DebugCommands.java
@@ -0,0 +1,13 @@
+package com.minetwice.phantomsmp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DebugCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("Debug mode toggled.");
+        return true;
+    }
+}
