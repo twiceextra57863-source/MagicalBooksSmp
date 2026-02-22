2a922391539eaf82625e32a0a00754cbfd49406b
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:52:56 +0530

    Create PlayerJoinListener.java

---
+++ src/main/java/com/minetwice/phantomsmp/listeners/PlayerJoinListener.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Welcome player logic
+    }
+}
