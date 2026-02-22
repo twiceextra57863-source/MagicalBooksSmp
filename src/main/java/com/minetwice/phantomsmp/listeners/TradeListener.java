410022692764efc66aca7d559714f9bedeb419aa
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:53:02 +0530

    Create TradeListener.java

---
+++ src/main/java/com/minetwice/phantomsmp/listeners/TradeListener.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class TradeListener implements Listener {
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        // Handle trade inventory clicks
+    }
+}
