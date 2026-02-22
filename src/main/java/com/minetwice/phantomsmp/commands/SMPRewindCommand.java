2beb1e9d76206c85614385e5559dfc5145f4b745
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 18:10:19 +0530

    Update SMPRewindCommand.java

--- src/main/java/com/minetwice/phantomsmp/commands/SMPRewindCommand.java
+++ src/main/java/com/minetwice/phantomsmp/commands/SMPRewindCommand.java
@@ -1,13 +1,66 @@
 package com.minetwice.phantomsmp.commands;
 
+import com.minetwice.phantomsmp.PhantomSMP;
+import com.minetwice.phantomsmp.utils.MessageUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class SMPRewindCommand implements CommandExecutor {
+    
+    private final PhantomSMP plugin;
+    
+    public SMPRewindCommand(PhantomSMP plugin) {
+        this.plugin = plugin;
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("SMP Rewound!");
+        if (!sender.hasPermission("phantomsmp.admin")) {
+            sender.sendMessage(MessageUtils.format("&cYou don't have permission!"));
+            return true;
+        }
+        
+        // Confirm argument
+        if (args.length < 1 || !args[0].equalsIgnoreCase("confirm")) {
+            sender.sendMessage(MessageUtils.format("&c&lWARNING: &7This will remove all power books from all players!"));
+            sender.sendMessage(MessageUtils.format("&eUse &6/smprewind confirm &eto proceed."));
+            return true;
+        }
+        
+        // Remove books from all online players
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            removePowerBooks(player);
+            plugin.getGemManager().removePlayerBook(player.getUniqueId());
+            player.sendMessage(MessageUtils.format("&cYour power book has been removed by an admin."));
+        }
+        
+        // Cancel grace period if active
+        plugin.getGraceManager().cancelGracePeriod();
+        
+        // Broadcast
+        Bukkit.broadcast(MessageUtils.format("&c&lSMP REWIND &8» &7All power books have been reset by " + sender.getName()));
+        
         return true;
+    }
+    
+    private void removePowerBooks(Player player) {
+        // Remove from inventory
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && plugin.getGemManager().isPowerBook(item)) {
+                item.setType(Material.AIR);
+            }
+        }
+        
+        // Remove from armor/offhand
+        for (ItemStack item : player.getInventory().getExtraContents()) {
+            if (item != null && plugin.getGemManager().isPowerBook(item)) {
+                item.setType(Material.AIR);
+            }
+        }
     }
 }
