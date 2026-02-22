84988f5f6843b1cea8c4bc850c5fbd6bd0250159
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 18:06:26 +0530

    Update MessageUtils.java

--- src/main/java/com/minetwice/phantomsmp/utils/MessageUtils.java
+++ src/main/java/com/minetwice/phantomsmp/utils/MessageUtils.java
@@ -1,9 +1,34 @@
 package com.minetwice.phantomsmp.utils;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class MessageUtils {
-    public static String color(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+    
+    private static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer.legacyAmpersand();
+    
+    public static Component format(String message) {
+        return SERIALIZER.deserialize(message);
+    }
+    
+    public static String stripColor(String message) {
+        return message.replaceAll("§[0-9a-fk-or]", "");
+    }
+    
+    public static Component prefix() {
+        return format("&8[&5PhantomSMP&8] &7");
+    }
+    
+    public static Component error(String message) {
+        return format("&c&lERROR &8» &7" + message);
+    }
+    
+    public static Component success(String message) {
+        return format("&a&lSUCCESS &8» &7" + message);
+    }
+    
+    public static Component info(String message) {
+        return format("&e&lINFO &8» &7" + message);
     }
 }
