9ba01103c63567abef0eb1a80f844d2f3881a25e
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:52:57 +0530

    Create PlayerQuitListener.java

---
+++ src/main/java/com/minetwice/phantomsmp/listeners/PlayerQuitListener.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // Handle quit logic
+    }
+}
