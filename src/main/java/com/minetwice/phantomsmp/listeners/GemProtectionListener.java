c72fb24067b19e0b4eeee2d461b1765b67d15996
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:52:58 +0530

    Create GemProtectionListener.java

---
+++ src/main/java/com/minetwice/phantomsmp/listeners/GemProtectionListener.java
@@ -0,0 +1,12 @@
+package com.minetwice.phantomsmp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class GemProtectionListener implements Listener {
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        // Handle gem protection logic
+    }
+}
