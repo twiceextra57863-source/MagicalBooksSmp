d2e9f709c2b0c0a164e8f4d8c99f9532c439a7b7
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-22 17:59:32 +0530

    Update PhantomSMP.java

--- src/main/java/com/minetwice/phantomsmp/PhantomSMP.java
+++ src/main/java/com/minetwice/phantomsmp/PhantomSMP.java
@@ -2,33 +2,112 @@ package com.minetwice.phantomsmp;
 
 import com.minetwice.phantomsmp.commands.*;
 import com.minetwice.phantomsmp.listeners.*;
+import com.minetwice.phantomsmp.managers.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PhantomSMP extends JavaPlugin {
-
+public class PhantomSMP extends JavaPlugin {
+    
+    private static PhantomSMP instance;
+    private DataManager dataManager;
+    private GemManager gemManager;
+    private CooldownManager cooldownManager;
+    private TradeManager tradeManager;
+    private GraceManager graceManager;
+    private ParticleManager particleManager;
+    private AnimationManager animationManager;
+    
     @Override
     public void onEnable() {
-        // Register Commands
-        if (getCommand("smpstart") != null) getCommand("smpstart").setExecutor(new SMPStartCommand());
-        if (getCommand("smprewind") != null) getCommand("smprewind").setExecutor(new SMPRewindCommand());
-        if (getCommand("gems") != null) getCommand("gems").setExecutor(new GemsCommand());
-        if (getCommand("gemgive") != null) getCommand("gemgive").setExecutor(new GemGiveCommand());
-        if (getCommand("trade") != null) getCommand("trade").setExecutor(new TradeCommand());
-        if (getCommand("debug") != null) getCommand("debug").setExecutor(new DebugCommands());
-
-        // Register Listeners
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
-        getServer().getPluginManager().registerEvents(new GemProtectionListener(), this);
-        getServer().getPluginManager().registerEvents(new AbilityListener(), this);
-        getServer().getPluginManager().registerEvents(new CombatListener(), this);
-        getServer().getPluginManager().registerEvents(new TradeListener(), this);
-
-        getLogger().info("PhantomSMP has been enabled!");
+        instance = this;
+        
+        // Save default config
+        saveDefaultConfig();
+        
+        // Initialize managers
+        this.dataManager = new DataManager(this);
+        this.gemManager = new GemManager(this);
+        this.cooldownManager = new CooldownManager(this);
+        this.tradeManager = new TradeManager(this);
+        this.graceManager = new GraceManager(this);
+        this.particleManager = new ParticleManager(this);
+        this.animationManager = new AnimationManager(this);
+        
+        // Register commands
+        registerCommands();
+        
+        // Register listeners
+        registerListeners();
+        
+        // Load data
+        dataManager.loadAll();
+        
+        getLogger().info("PhantomSMP v" + getDescription().getVersion() + " enabled!");
+        getLogger().info("Author: MineTwice");
     }
-
+    
     @Override
     public void onDisable() {
-        getLogger().info("PhantomSMP has been disabled!");
+        // Save all data
+        if (dataManager != null) {
+            dataManager.saveAll();
+        }
+        
+        // Cancel all tasks
+        getServer().getScheduler().cancelTasks(this);
+        
+        getLogger().info("PhantomSMP disabled!");
+    }
+    
+    private void registerCommands() {
+        getCommand("smpstart").setExecutor(new SMPStartCommand(this));
+        getCommand("smprewind").setExecutor(new SMPRewindCommand(this));
+        getCommand("books").setExecutor(new GemsCommand(this));
+        getCommand("bookgive").setExecutor(new GemGiveCommand(this));
+        getCommand("trade").setExecutor(new TradeCommand(this));
+        getCommand("bookinfo").setExecutor(new DebugCommands(this));
+        getCommand("setbooklevel").setExecutor(new DebugCommands(this));
+        getCommand("addkills").setExecutor(new DebugCommands(this));
+        getCommand("resetcooldowns").setExecutor(new DebugCommands(this));
+    }
+    
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
+        getServer().getPluginManager().registerEvents(new GemProtectionListener(this), this);
+        getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
+        getServer().getPluginManager().registerEvents(new CombatListener(this), this);
+        getServer().getPluginManager().registerEvents(new TradeListener(this), this);
+    }
+    
+    public static PhantomSMP getInstance() {
+        return instance;
+    }
+    
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+    
+    public GemManager getGemManager() {
+        return gemManager;
+    }
+    
+    public CooldownManager getCooldownManager() {
+        return cooldownManager;
+    }
+    
+    public TradeManager getTradeManager() {
+        return tradeManager;
+    }
+    
+    public GraceManager getGraceManager() {
+        return graceManager;
+    }
+    
+    public ParticleManager getParticleManager() {
+        return particleManager;
+    }
+    
+    public AnimationManager getAnimationManager() {
+        return animationManager;
     }
 }
