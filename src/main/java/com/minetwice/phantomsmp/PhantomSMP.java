a4d7a27276040fdd50e7f416686a326408e77586
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-23 07:11:32 +0530

    Update PhantomSMP.java

--- src/main/java/com/minetwice/phantomsmp/PhantomSMP.java
+++ src/main/java/com/minetwice/phantomsmp/PhantomSMP.java
@@ -1,163 +1,67 @@
 package com.minetwice.phantomsmp;
 
-import com.minetwice.phantomsmp.commands.*;
-import com.minetwice.phantomsmp.listeners.*;
-import com.minetwice.phantomsmp.managers.*;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PhantomSMP extends JavaPlugin {
-
-    private static PhantomSMP instance;
-    private BukkitAudiences adventure;
     
-    // Managers
-    private DataManager dataManager;
-    private GemManager gemManager;
-    private CooldownManager cooldownManager;
-    private TradeManager tradeManager;
-    private GraceManager graceManager;
-    private ParticleManager particleManager;
-    private AnimationManager animationManager;
-
+    private static PhantomSMP instance;
+    
     @Override
     public void onEnable() {
         instance = this;
         
-        // Log startup
-        getLogger().info("§ePhantomSMP is starting...");
+        // Display startup message
+        getLogger().info("+=======================================+");
+        getLogger().info("|     PhantomSMP v" + getDescription().getVersion() + "           |");
+        getLogger().info("|     Author: MineTwice                 |");
+        getLogger().info("|     Server: Paper 1.21.4              |");
+        getLogger().info("|     Status: LOADED SUCCESSFULLY       |");
+        getLogger().info("+=======================================+");
         
-        try {
-            // Initialize Adventure
-            this.adventure = BukkitAudiences.create(this);
-            
-            // Save default config
-            saveDefaultConfig();
-            
-            // Initialize managers
-            getLogger().info("§7Initializing managers...");
-            this.dataManager = new DataManager(this);
-            this.gemManager = new GemManager(this);
-            this.cooldownManager = new CooldownManager(this);
-            this.tradeManager = new TradeManager(this);
-            this.graceManager = new GraceManager(this);
-            this.particleManager = new ParticleManager(this);
-            this.animationManager = new AnimationManager(this);
-            
-            // Register commands
-            registerCommands();
-            
-            // Register listeners
-            registerListeners();
-            
-            // Load all player data
-            if (dataManager != null) {
-                dataManager.loadAll();
-            }
-            
-            getLogger().info("§a╔════════════════════════════════════╗");
-            getLogger().info("§a║     PhantomSMP v" + getDescription().getVersion() + " Enabled      ║");
-            getLogger().info("§a║        Author: MineTwice           ║");
-            getLogger().info("§a║      Server: 1.21.4 Compatible     ║");
-            getLogger().info("§a╚════════════════════════════════════╝");
-            
-        } catch (Exception e) {
-            getLogger().severe("§cFailed to enable PhantomSMP: " + e.getMessage());
-            e.printStackTrace();
-            getServer().getPluginManager().disablePlugin(this);
+        // Check server software
+        String serverName = Bukkit.getServer().getName();
+        String serverVersion = Bukkit.getBukkitVersion();
+        
+        getLogger().info("§aServer Software: " + serverName);
+        getLogger().info("§aServer Version: " + serverVersion);
+        
+        if (serverName.equalsIgnoreCase("Paper")) {
+            getLogger().info("§a✓ Running on Paper - Optimized!");
         }
     }
-
+    
     @Override
     public void onDisable() {
-        getLogger().info("§ePhantomSMP is shutting down...");
-        
-        try {
-            if (dataManager != null) {
-                dataManager.saveAll();
-                dataManager.close();
+        getLogger().info("PhantomSMP has been disabled!");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("phantomtest")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage("§a§l╔════════════════════════════════╗");
+                player.sendMessage("§a§l║    PhantomSMP is WORKING!     ║");
+                player.sendMessage("§a§l╠════════════════════════════════╣");
+                player.sendMessage("§f  Version: §e" + getDescription().getVersion());
+                player.sendMessage("§f  Author: §eMineTwice");
+                player.sendMessage("§f  Server: §ePaper 1.21.4");
+                player.sendMessage("§a§l╚════════════════════════════════╝");
+            } else {
+                sender.sendMessage("PhantomSMP is working!");
+                sender.sendMessage("Version: " + getDescription().getVersion());
+                sender.sendMessage("Server: Paper 1.21.4");
             }
-            
-            if (adventure != null) {
-                adventure.close();
-            }
-            
-            getServer().getScheduler().cancelTasks(this);
-            
-        } catch (Exception e) {
-            getLogger().severe("§cError during disable: " + e.getMessage());
+            return true;
         }
-        
-        getLogger().info("§cPhantomSMP disabled!");
+        return false;
     }
-
-    private void registerCommands() {
-        try {
-            if (getCommand("smpstart") != null)
-                getCommand("smpstart").setExecutor(new SMPStartCommand(this));
-            if (getCommand("smprewind") != null)
-                getCommand("smprewind").setExecutor(new SMPRewindCommand(this));
-            if (getCommand("books") != null)
-                getCommand("books").setExecutor(new GemsCommand(this));
-            if (getCommand("bookgive") != null)
-                getCommand("bookgive").setExecutor(new GemGiveCommand(this));
-            if (getCommand("trade") != null)
-                getCommand("trade").setExecutor(new TradeCommand(this));
-                
-            getLogger().info("§a✓ Commands registered successfully");
-        } catch (Exception e) {
-            getLogger().warning("§cFailed to register some commands: " + e.getMessage());
-        }
-    }
-
-    private void registerListeners() {
-        try {
-            getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-            getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-            getServer().getPluginManager().registerEvents(new GemProtectionListener(this), this);
-            getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
-            getServer().getPluginManager().registerEvents(new CombatListener(this), this);
-            getServer().getPluginManager().registerEvents(new TradeListener(this), this);
-            
-            getLogger().info("§a✓ Listeners registered successfully");
-        } catch (Exception e) {
-            getLogger().warning("§cFailed to register some listeners: " + e.getMessage());
-        }
-    }
-
+    
     public static PhantomSMP getInstance() {
         return instance;
-    }
-
-    public BukkitAudiences getAdventure() {
-        return adventure;
-    }
-
-    public DataManager getDataManager() {
-        return dataManager;
-    }
-
-    public GemManager getGemManager() {
-        return gemManager;
-    }
-
-    public CooldownManager getCooldownManager() {
-        return cooldownManager;
-    }
-
-    public TradeManager getTradeManager() {
-        return tradeManager;
-    }
-
-    public GraceManager getGraceManager() {
-        return graceManager;
-    }
-
-    public ParticleManager getParticleManager() {
-        return particleManager;
-    }
-
-    public AnimationManager getAnimationManager() {
-        return animationManager;
     }
 }
