58ef112ac420287a7352e9bf1347b87e350884e5
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-23 06:21:00 +0530

    Update MessageUtils.java

--- src/main/java/com/minetwice/phantomsmp/utils/MessageUtils.java
+++ src/main/java/com/minetwice/phantomsmp/utils/MessageUtils.java
@@ -1,34 +1,54 @@
 package com.minetwice.phantomsmp.utils;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.ChatColor;
 
 public class MessageUtils {
     
-    public static String format(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
+    private static final LegacyComponentSerializer LEGACY_SERIALIZER = LegacyComponentSerializer.legacySection();
     
     public static String colorize(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
     
+    public static Component toComponent(String message) {
+        return LEGACY_SERIALIZER.deserialize(colorize(message));
+    }
+    
     public static String stripColor(String message) {
-        return ChatColor.stripColor(message);
+        return ChatColor.stripColor(colorize(message));
     }
     
     public static String prefix() {
-        return format("&8[&5PhantomSMP&8] &7");
+        return colorize("&8[&5PhantomSMP&8] &7");
+    }
+    
+    public static Component prefixComponent() {
+        return toComponent("&8[&5PhantomSMP&8] &7");
     }
     
     public static String error(String message) {
-        return format("&c&lERROR &8» &7" + message);
+        return colorize("&c&lERROR &8» &7" + message);
+    }
+    
+    public static Component errorComponent(String message) {
+        return toComponent("&c&lERROR &8» &7" + message);
     }
     
     public static String success(String message) {
-        return format("&a&lSUCCESS &8» &7" + message);
+        return colorize("&a&lSUCCESS &8» &7" + message);
+    }
+    
+    public static Component successComponent(String message) {
+        return toComponent("&a&lSUCCESS &8» &7" + message);
     }
     
     public static String info(String message) {
-        return format("&e&lINFO &8» &7" + message);
+        return colorize("&e&lINFO &8» &7" + message);
+    }
+    
+    public static Component infoComponent(String message) {
+        return toComponent("&e&lINFO &8» &7" + message);
     }
 }
